a94d4514fbf544488d7df1d4d80edf01fde7c931
BullGator92 <38494083+BullGator92@users.noreply.github.com>
2018-11-26 23:49:37 -0500

    11-26 update

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,30 +1,19 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
+// path is required since it is used on this page in the function
 var path = require("path");
 
+// Routing
+module.exports = function htmlRoutes(app) {
+    // HTML GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an HTML page of content
+    // A GET Route to /survey which should display the survey page.
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+    // A default USE route that leads to home.html which displays the home page.
+    // Basic route that sends the user first to the AJAX Page
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
-module.exports = function(app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
-
-  app.get("/home", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
-
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
-  });
-
-  // If no matching route is found default to home
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
 };
