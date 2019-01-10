175e0e530ff2fec6cb5d779455c07396e93d92ad
BullGator92 <38494083+BullGator92@users.noreply.github.com>
2019-01-10 01:38:00 -0500

    updates 1/9

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,25 +1,24 @@
-// Preloaded data in the app is displayed as an array of objects
+
 var friends = [{
-    "name": "Sergey",
-    "photo": "http://i.huffpost.com/gen/1358010/images/o-SERGEY-BRIN-facebook.jpg",
+    "name": "Ross Geller",
+    "photo": "https://gl-images.condecdn.net/image/jLpYBeXRj73/crop/1020/f/Friends17_glamour_17apr13_NBCUniversal_getty_b.jpg",
     "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
 }, {
-    "name": "Alan",
-    "photo": "http://a2.files.biography.com/image/upload/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE5NDg0MDU1MTUzMTE2Njg3.jpg",
+    "name": "Joey Tribbiani",
+    "photo": "https://fnwonline.com/wp-content/uploads/2018/01/Joey-Tribbiani.png",
     "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
 }, {
-    "name": "Grace",
-    "photo": "http://history-computer.com/ModernComputer/Software/images/Hopper_young.jpg",
+    "name": "Monica Geller",
+    "photo": "https://sites.google.com/site/friends2365/_/rsrc/1472782816715/home/monica-geller/monica.jpg",
     "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
 }, {
-    "name": "John",
-    "photo": "https://www.wired.com/wp-content/uploads/blogs/wiredenterprise/wp-content/uploads/2011/10/john-mccarthy.png",
+    "name": "Chandler Bing",
+    "photo": "https://friends4advait.files.wordpress.com/2007/08/perry2.jpg?w=500",
     "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
 }, {
-    "name": "Ada",
-    "photo": "http://vignette1.wikia.nocookie.net/curious-expedition/images/a/a0/Lovelace.jpg/revision/latest?cb=20160129184107",
+    "name": "Rachel Green",
+    "photo": "https://hips.hearstapps.com/cosmouk.cdnds.net/16/41/480x558/gallery-1476291160-jennifer-aniston-friends.jpg?resize=*:2814",
     "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
 }];
 
-// friends object returned as the result of a require call
 module.exports = friends;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,19 +1,13 @@
-// path is required since it is used on this page in the function
 var path = require("path");
 
 // Routing
 module.exports = function htmlRoutes(app) {
-    // HTML GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases the user is shown an HTML page of content
-    // A GET Route to /survey which should display the survey page.
-    app.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
-    });
-    // A default USE route that leads to home.html which displays the home page.
-    // Basic route that sends the user first to the AJAX Page
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
+  // HTML GET Requests
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
 
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
 };
